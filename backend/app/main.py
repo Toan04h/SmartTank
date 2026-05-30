dce1ad8f970e438c1feb1184230d990f319926db
Toan Huynh <142817952+Toan04h@users.noreply.github.com>
2026-05-30 16:40:43 -0500

    chore: reconfig CORS to support vercel url

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://smart-tank-vercel-app.vercel.app",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
